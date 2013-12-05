22e76ad0cc3d140ddc699baffde3fc7a14d008d9
Sriram Sundarraj <sriram.s.1994@gmail.com>
2013-12-05 14:00:56 +0530

    Got date.

--- script.rb
+++ script.rb
@@ -14,9 +14,11 @@ result.search("//div[@class='reviewHeading']").each do |heading|
 	h['title']=heading.content.gsub("\n", "")
 end
 
-result.search("//div[@style='color:#000000; font-size:12px;']").each do |rev|
-	h['content']=rev.content.gsub("\n", " ")
+result.search("//div[@style='color:#000000; font-size:12px;']").each do |text|
+	h['text']=text.content.gsub("\n", " ")
+end
+result.search("//div[@style='color:#656565; font-size:11px; width:115px;']").each do |dt|
+	h['date']=dt.content.gsub("\n", " ")
 	array.push(h)
 end
-
 puts array
